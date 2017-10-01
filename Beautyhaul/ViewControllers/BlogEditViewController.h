5977fa07977c51177b36c0959153626cb4d0fbbc
liuweiliang <liuweiliang@liuweiliangsMBP.lan>
2017-10-01 18:12:33 +0800

    add blog edit view controller

---
+++ Beautyhaul/ViewControllers/BlogEditViewController.h
@@ -0,0 +1,13 @@
+//
+//  BlogEditViewController.h
+//  Beautyhaul
+//
+//  Created by liuweiliang on 2017/9/21.
+//  Copyright © 2017年 beauty-haul. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BlogEditViewController : UIViewController
+
+@end
