c6e4aefaf87b713b4cc1636f37ca9b2f61f67598
liuweiliang <liuweiliang@liuweiliangsMBP.lan>
2017-10-01 18:15:13 +0800

    1.set top text and button color 2.enable interactivePopGesture

--- Beautyhaul/ViewControllers/NavigationViewController.m
+++ Beautyhaul/ViewControllers/NavigationViewController.m
@@ -8,7 +8,7 @@
 
 #import "NavigationViewController.h"
 
-@interface NavigationViewController ()
+@interface NavigationViewController ()<UIGestureRecognizerDelegate>
 
 @end
 
@@ -16,6 +16,10 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    [self.navigationBar setTintColor:[UIColor colorWithWhite:0.4 alpha:1.0]];
+    [self.navigationBar setTitleTextAttributes:@{NSForegroundColorAttributeName : [UIColor colorWithWhite:0.4 alpha:1.],
+                                                 NSFontAttributeName:[UIFont fontWithName:@"Helvetica" size:17.]}];
+    self.interactivePopGestureRecognizer.delegate = self;
 }
 
 - (void)didReceiveMemoryWarning {
@@ -26,6 +30,15 @@
 - (void)pushViewController:(UIViewController *)viewController animated:(BOOL)animated{
     viewController.hidesBottomBarWhenPushed = self.viewControllers.count > 0;
     [super pushViewController:viewController animated:animated];
+}
+
+#pragma mark - UIGestureRecognizerDelegate
+- (BOOL)gestureRecognizerShouldBegin:(UIGestureRecognizer *)gestureRecognizer{
+    return self.viewControllers.count > 1;
+}
+
+- (BOOL)gestureRecognizer:(UIGestureRecognizer *)gestureRecognizer shouldRecognizeSimultaneouslyWithGestureRecognizer:(UIGestureRecognizer *)otherGestureRecognizer{
+    return YES;
 }
 
 @end
